2c99ab6cc285f21b69d7a2a7dd9fc6f93af3f41c
Arshath Ahamed A <121730585+arshathahamed10@users.noreply.github.com>
2024-05-31 12:17:52 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -44,27 +44,115 @@ class Pair{
         this.dist = b;
     }
 }
+class Edge implements Comparable<Edge>{
+    int src,dest,weight;
+    public Edge(int a,int b,int c){
+        this.src = a;
+        this.dest = b;
+        this.weight = c;
+    }
+    public int compareTo(Edge compareEdge){
+        return this.weight - compareEdge.weight;
+    }
+}
+class DisjointSet{
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n){
+        for(int i = 0 ; i <= n;i++){
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    public int findUltimateParent(int u){
+        if(parent.get(u) == u) return u;
+        int ultPar = findUltimateParent(parent.get(u));
+        parent.set(u,ultPar);
+        return parent.get(u);
+    }
+    public void unionByRank(int u,int v){
+        int ul_u = findUltimateParent(u);
+        int ul_v = findUltimateParent(v);
+        if(ul_u == ul_v) return;
+        if(rank.get(ul_u) < rank.get(ul_v)){
+            parent.set(ul_u,ul_v);
+        }
+        else if(rank.get(ul_v) < rank.get(ul_u)){
+            parent.set(ul_v,ul_u);
+        }
+        else{
+            parent.set(ul_v,ul_u);
+            rank.set(ul_v,rank.get(ul_u)+1);
+        }
+    }
+    public void unionBySize(int u,int v){
+        int ul_u = findUltimateParent(u);
+        int ul_v = findUltimateParent(v);
+        if(ul_u == ul_v) return;
+        if(size.get(ul_u) < size.get(ul_v)){
+            parent.set(ul_u,ul_v);
+            size.set(ul_v,size.get(ul_v)+size.get(ul_u));
+        }
+        else{
+            parent.set(ul_v,ul_u);
+            size.set(ul_u,size.get(ul_u) + size.get(ul_v));
+        }
+    }
+}
 class Solution {
     static int spanningTree(int V, int E, List<List<int[]>> adj) {
         // Code Here.
-        int[] visit = new int[V];
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.dist - b.dist);
-        pq.add(new Pair(0,0));
-        int sum = 0;
-        while(!pq.isEmpty()){
-            Pair min = pq.peek();
-            pq.remove();
-            int node = min.node;
-            int dist = min.dist;
-            if(visit[node] == 1) continue;
-            visit[node] = 1;
-            sum += dist;
-            for(int i = 0 ; i < adj.get(node).size() ; i++){
-                int edgW = adj.get(node).get(i)[1];
-                int adjNode = adj.get(node).get(i)[0];
-                if(visit[adjNode] == 0) pq.add(new Pair(adjNode,edgW));
+        // int[] visit = new int[V];
+        // for(List<int[]> a:adj){
+        //     for(int[] b : a){
+        //         System.out.println(Arrays.toString(b));
+        //     }
+        //     System.out.println("Finish");
+        // }
+        // PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.dist - b.dist);
+        // pq.add(new Pair(0,0));
+        // int sum = 0;
+        // while(!pq.isEmpty()){
+        //     Pair min = pq.peek();
+        //     pq.remove();
+        //     int node = min.node;
+        //     int dist = min.dist;
+        //     if(visit[node] == 1) continue;
+        //     visit[node] = 1;
+        //     sum += dist;
+        //     for(int i = 0 ; i < adj.get(node).size() ; i++){
+        //         int edgW = adj.get(node).get(i)[1];
+        //         int adjNode = adj.get(node).get(i)[0];
+        //         if(visit[adjNode] == 0) pq.add(new Pair(adjNode,edgW));
+        //     }
+        // }
+        // return sum;
+        
+        
+        // Krushkals
+        List<Edge> edges = new ArrayList<>();
+        for(int i = 0 ; i < V;i++){
+            for(int j = 0; j < adj.get(i).size();j++){
+                int adjNode = adj.get(i).get(j)[0];
+                int adjWeight = adj.get(i).get(j)[1];
+                Edge edge = new Edge(i, adjNode, adjWeight);
+                edges.add(edge);
             }
         }
-        return sum;
+        DisjointSet ds = new DisjointSet(V);
+        Collections.sort(edges);
+        int weight = 0;
+        for(Edge edge : edges){
+            int u = edge.src;
+            int v = edge.dest;
+            int w = edge.weight;
+            if(ds.findUltimateParent(u) != ds.findUltimateParent(v)){
+                weight += w;
+                ds.unionBySize(u, v);
+            }
+        }
+        return weight;
     }
 }
