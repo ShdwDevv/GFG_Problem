4c581e05c50d585e3fe0c24d867da1cfe1ab996d
Arshath Ahamed A <121730585+ShdwDevv@users.noreply.github.com>
2024-09-09 14:20:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Page Faults in LRU/page-faults-in-lru.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int pages[]= new int[N];
+            for(int i = 0; i < N; i++)
+                pages[i] = Integer.parseInt(input_line[i]);
+            int C = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.pageFaults(N, C, pages));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static int pageFaults(int N, int C, int pages[]){
+        Deque<Integer> dq = new ArrayDeque<>();
+        int pageCount = 0;
+        for(int page : pages){
+            if(!dq.contains(page)){
+                if(dq.size() == C) dq.removeLast();
+                pageCount++;
+            }
+            else{
+                dq.remove(page);
+            }
+            dq.addFirst(page);
+        }
+        return pageCount;
+    }
+}
